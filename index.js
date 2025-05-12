e48fdd8ac0071bcc9a12eb4abcd1983a69cb84f2
Jesus Hernandez <jesushdzusa@gmail.com>
2025-05-12 17:28:19 -0500

    3.17*: Phonebook database, step 5

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/", (request, response) => {
 app.get("/api/persons", (request, response) => {
     Contact.find({}).then(persons => {
         response.send(persons)
-      
+
     })
 
 })
@@ -43,18 +43,18 @@ app.get("/info", (request, response) => {
     Contact.find({}).then(persons => {
         let info = infoPage(persons)
         response.send(info)
-        
+
     })
 
 })
 
-app.get("/api/persons/:id", (request, response,next) => {
+app.get("/api/persons/:id", (request, response, next) => {
     let id = request.params.id;
     Contact.findById(id)
-    .then(person => {
-        response.send(person)       
-    })
-    .catch(error=>next(error))
+        .then(person => {
+            response.send(person)
+        })
+        .catch(error => next(error))
     /*
     let personFound = persons.find(person => person.id === id)
     if (personFound) {
@@ -70,37 +70,48 @@ app.delete("/api/persons/:id", (request, response, next) => {
         response.status(204).end();
 
     })
-    .catch(error=>next(error))
+        .catch(error => next(error))
     //persons = persons.filter(person => person.id !== id);
 
 })
 
-app.post("/api/persons", (request, response,next) => {
+app.put("/api/persons/:id", (request, response, next) => {
+    const { number } = request.body
+    Contact.findById(request.params.id)
+        .then(person => {
+            if (!person) {
+                return response.status(404).end()
+            }
+            person.number = number;
+            return person.save().then((personUpdated) => {
+                response.json(personUpdated)
+            })
+        })
+        .catch(error => next(error))
+})
+
+app.post("/api/persons", (request, response, next) => {
     //let id = persons.length>0?Math.max(...persons.map(person=>Number(person.id))):0;
     let name = request.body.name;
-    let number = request.body.number;
-    if (!name || !number) {
-        return response.status(400).json({ error: "missing information" })
-    }
-    else {
-        Contact.findOne({ name: name }).then(result => {
-            if (result) {
-                console.log(result.name)
-                console.log(result.number)
-                return response.status(400).json({ error: "name must be unique" });
-            } else {
-                console.log(result)
-                let newPerson = new Contact( {
-                    name: request.body.name,
-                    number: request.body.number
-                })
-                newPerson.save().then(result=>{
-                    response.send(result)
-                })
+    Contact.findOne({ name: name }).then(result => {
+        if (result) {
+            console.log(result.name)
+            console.log(result.number)
+            return response.status(400).json({ error: "name must be unique" });
+        } else {
+            console.log(result)
+            let newPerson = new Contact({
+                name: request.body.name,
+                number: request.body.number
+            })
+            newPerson.save().then(result => {
+                response.send(result)
+            })
 
-            }
-        }).catch(error=>next(error))
-    }
+        }
+    })
+    .catch(error => next(error))
+
 
     /*
     let id = Math.floor(Math.random() * 1000000);
@@ -122,10 +133,10 @@ app.post("/api/persons", (request, response,next) => {
     response.json(newPerson)*/
 })
 
-const errorHandler=(error, request, response, next)=>{
+const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-    if(error.name === 'CastError'){
-        return response.status(400).send({error:'malformatted id'})
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
     }
     next(error)
 }
