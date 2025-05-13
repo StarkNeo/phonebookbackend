875d456e7080f69c1cbbfc9f11332b4def21d165
Jesus Hernandez <jesushdzusa@gmail.com>
2025-05-12 23:53:11 -0500

    3.20*: Phonebook database, step 8

--- models/phonebook.js
+++ models/phonebook.js
@@ -19,14 +19,21 @@ mongoose.connect(url)
 const contactSchema = new mongoose.Schema({
     name: {
         type: String,
-        minLength:3,
+        minLength: 3,
         required: true
     },
     number: {
         type: String,
-        required: String
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /\d{3}-\d{7}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`,
+        },
+        required: [true, 'User phone number required']
     }
-})
+});
 
 contactSchema.set('toJSON', {
     transform: (document, returnedObject) => {
